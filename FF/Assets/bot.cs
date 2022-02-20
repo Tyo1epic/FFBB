899ad7f9fe97eb21306e8a3cb9469d50f6640471
Tyo1epic <jhua66285@gmail.com>
2022-02-20 08:08:44 +0800

    second floor added

--- FF/Assets/bot.cs
+++ FF/Assets/bot.cs
@@ -18,19 +18,33 @@ public class bot : MonoBehaviour
     {
         RaycastHit hit;
         Ray lookWall = new Ray(transform.position, transform.forward);
-        Vector3 direction = player.position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(direction);
-        Vector3 directions = player.position - transform.GetChild(0).position;
-        Quaternion rotations = Quaternion.LookRotation(directions);
+         Vector3 direction = player.position - transform.position;
+ Quaternion rotation = Quaternion.LookRotation(direction);
+ Vector3 directions = player.position - transform.GetChild(0).position;
+ Quaternion rotations = Quaternion.LookRotation(directions);
+        if(hey == true){
+        directions = lastplayer.position - transform.GetChild(0).position;
+        rotations = Quaternion.LookRotation(directions);
         transform.rotation = rotation;
         transform.GetChild(0).rotation = rotations;
+        }else{
+        transform.rotation = rotation;
+        transform.GetChild(0).rotation = rotations;
+        }
+
         transform.position += (transform.forward * 2 * Time.deltaTime);
+        if(direction.y > 5){
+            GameObject[] res = GameObject.FindGameObjectsWithTag("stairs");
+            player = res[Random.Range(0, res.Length)].transform;
+            hey = true;
+        }
         if(direction.x < 0.3f && direction.y < 0.3f && direction.z < 0.3f && hey == true){
 player = lastplayer;
+hey = false;
         }
         if(Physics.Raycast(lookWall, out hit, 3)){
             Debug.Log(hit.collider.tag);
-            if(hit.collider.tag == "wwall"){
+            if(hit.collider.tag == "wwall" || hit.collider.tag == "stair"){
                 if(hit.collider.transform.childCount > 0){
 player = hit.collider.transform.GetChild(0);
 hey = true;
